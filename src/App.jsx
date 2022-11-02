4f3e1ea0ac7486fcc495f60f54638a9be66037b2
rishabhkr-r111 <rishabhkrr111@gmail.com>
2022-11-02 16:19:19 +0530

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ return (
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {/* <button
+                {/* <butto  n
                   type="button"
                   className="rounded-full bg-gray-900 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
