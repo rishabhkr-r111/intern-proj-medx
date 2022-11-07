bda7ad192f89fbbed68b2049aa4c6d1be2eb80ed
RahulDixit23 <115360872+RahulDixit23@users.noreply.github.com>
2022-11-07 11:52:37 +0530

    1 file modified

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,36 @@
-
 function About() {
-
-    return(
-        <>
-            <h1>about</h1>
-        </>
-    )
+  return (
+    <>
+      <h1 className=" text-center m-8 text-4xl font-semibold text-gray-600">
+        What is MedX?
+      </h1>
+      <p className=" text-center m-8 text-[20px] ">
+        MedX is the most user-friendly app for ordering medications that you can
+        find. In order to deliver better healthcare at lower costs, we bring
+        together a variety of pharmaceutical businesses, diagnostic labs,
+        healthcare goods, and clinicians with the highest levels of experience.
+        As a result, our clients receive the best service possible throughout
+        India.
+      </p>
+      <p className=" text-center m-8 text-[18px] italic  text-green-600 ">
+        {' '}
+        Healthcare made easy by the pharmacy you trust
+      </p>
+      <p className=" text-center m-8 text-4xl font-semibold text-gray-600">
+        {' '}
+        Our Services
+      </p>
+      <p className=" text-center m-8 text-[20px] ">
+        We provide consumers with the option of drone delivery, which allows
+        them to obtain their things in minutes. As a result, we offer
+        contactless payments, allowing them to stay safe in the comfort of their
+        own homes. Booking online appointments and meeting doctors who are both
+        qualified and experienced has never been easier with MedX. Additionally,
+        we provide lab tests to clients at their homes, helping them to monitor
+        their health.
+      </p>
+    </>
+  );
 }
 
-export default About
+export default About;
