8eb73ea4e9f19301dfd4b3f21d18c8e2e5adac4b
rishabhkr-r111 <rishabhkrr111@gmail.com>
2022-11-07 13:52:50 +0530

    consers

---
+++ src/assets/comp/concerns.jsx
@@ -0,0 +1,108 @@
+
+function Concerns() {
+    return(
+        <>
+        <div className="flex"> 
+            <div className="mt-7 mb-2 mx-9 text-xl text-white font-bold text-center p-2  rounded-[25px]
+              bg-black drop-shadow-xl px-7 py-2
+            ">  Shop by Concerns  </div>
+        </div>
+
+        <div className="w-full flex overflow-x-scroll flex-nowrap no-scrollbar">
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284288640163930/png_20221104_232053_0000.png"
+            />
+            <div className="product-text">Diabetes</div>
+
+            </div>
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284289118318602/png_20221104_233207_0000.png"
+            />
+            <div className="product-text">Cardiac</div>
+
+            </div>
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284289512591530/png_20221105_002546_0000.png"
+            />
+            <div className="product-text">Gastro Intestinal</div>
+
+            </div>
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284289936207883/png_20221105_073031_0000.png"
+            />
+            <div className="product-text">Liver</div>
+
+            </div>
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284290275954698/png_20221105_073903_0000.png"
+            />
+            <div className="product-text">Respiratory</div>
+
+            </div>
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284291047694376/png_20221105_074534_0000.png"
+            />
+            <div className="product-text">Bones, Joints And Muscles</div>
+
+            </div>
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284291509071972/png_20221105_081259_0000.png"
+            />
+            <div className="product-text">ENT</div>          
+
+            </div>
+
+           
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284292326969414/png_20221105_082022_0000.png"
+            />
+            <div className="product-text">Eye</div>          
+            </div>
+
+            <div className="product-container ">
+            <img 
+                className="product-type"
+                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284291894943824/png_20221105_081508_0000.png"
+            />
+            <div className="product-text">Kidney</div>          
+            </div>
+
+            
+
+           
+
+
+        </div>
+        
+        
+        
+        </>
+    )
+
+}
+
+export default Concerns
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import Hero from '../assets/comp/hero'
 import Search from '../assets/comp/search';
 import ProductTypes from '../assets/comp/productType';
 import WhyUs from '../assets/comp/whyUs';
+import Concerns from '../assets/comp/concerns';
 
 function Home () {
 
@@ -10,7 +11,9 @@ function Home () {
           <Search/>
           <Hero />
           <ProductTypes />
+          <Concerns />
           <WhyUs />
+
         </>
     )
 }
