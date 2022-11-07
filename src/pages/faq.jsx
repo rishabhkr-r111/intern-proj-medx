9eb5680d6b52fbd3040b8da92ae81005355aa5eb
NarayananSreeraman <1MJ21CS129@mvjce.edu.in>
2022-11-08 00:39:15 +0530

    EditsBroo

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -6,6 +6,7 @@ function Faq() {
              <div className="flex w-full "> 
             <div className="mt-7 mb-2 mx-9 text-xl text-white font-bold text-center p-2  rounded-[25px]
               bg-black drop-shadow-xl px-7 py-2">  FAQs </div>
+              
         </div>
         </>
     )
