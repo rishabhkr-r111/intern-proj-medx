1dbb1a9905e7746ecbb93f9187f0dbb2ba65a969
NarayananSreeraman <1MJ21CS129@mvjce.edu.in>
2022-11-07 23:30:54 +0530

    Changesv.1

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -5,8 +5,7 @@ function Faq() {
         <>
              <div className="flex w-full "> 
             <div className="mt-7 mb-2 mx-9 text-xl text-white font-bold text-center p-2  rounded-[25px]
-              bg-black drop-shadow-xl px-7 py-2 
-            ">  FAQs </div>
+              bg-black drop-shadow-xl px-7 py-2">  FAQs </div>
         </div>
         </>
     )
