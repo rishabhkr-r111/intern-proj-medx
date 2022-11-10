cd6becb9eb8f3a560adf7bfca1775560fd150126
Pranav Chandran <pranavchandran250604@gmail.com>
2022-11-10 12:15:07 +0530

    Contact FAQ Updates

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
               <h3 className="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
-              <p className="mt-1 text-sm text-gray-600">Use a permanent address where you can receive mail.</p>
+              <p className="mt-1 text-sm text-gray-600">Get in touch!</p>
             </div>
           </div>
 
