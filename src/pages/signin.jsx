1b6fecb579f69f7d9be29dfe93dc3a0fdb26e90a
rishabhkr-r111 <rishabhkrr111@gmail.com>
2022-11-19 09:15:33 +0530

    bug fixes

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,121 +1,134 @@
-import { LockClosedIcon } from '@heroicons/react/20/solid'
-import { Link, useNavigate } from 'react-router-dom'
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../firebase-config'
-import { useState } from 'react'
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase-config";
+import { useState } from "react";
 
 function Signin() {
-  const navigate = useNavigate()
-  const [email, setemail] = useState('');
-  const [password, setpassword] = useState('');
+  const navigate = useNavigate();
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
 
   const login = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const user = await signInWithEmailAndPassword(auth, email, password)
-      console.log(user)
-      navigate('/')
-
+      const user = await signInWithEmailAndPassword(auth, email, password);
+      console.log(user);
+      navigate(-1);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-
-
-    return(
-        <>
-            <div >
-                <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
-        <div className="w-full max-w-md space-y-8 border-2 border-gray-200 rounded-xl drop-shadow-lg bg-gray-50 p-11">
-          <div>
-            <img
-              className="mx-auto h-24 w-auto"
-              src="https://cdn.discordapp.com/attachments/1037406020449861724/1037797448900419614/20221104_000510_0000.png"
-              alt="Your Company"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{' '}
-              <Link to="/register" className="font-medium text-blue-600 hover:text-blue-500">
-                register for new account
-              </Link>
-            </p>
-          </div>
-          <form className="mt-8 space-y-6" >
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Email address"
-                  onChange={(e) => { setemail(e.target.value) }}
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Password"
-                   onChange={(e) => { setpassword(e.target.value) }}
-                />
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                />
-                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                  Remember me
-                </label>
-              </div>
-
-              <div className="text-sm">
-                <a href="#" className="font-medium text-green-600 hover:text-green-500">
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-
+  return (
+    <>
+      <div>
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
+          <div className="w-full max-w-md space-y-8 border-2 border-gray-200 rounded-xl drop-shadow-lg bg-gray-50 p-11">
             <div>
-              <button
-                onClick={ login }
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-green-200 group-hover:text-hover-500" aria-hidden="true" />
-                </span>
-                Sign in
-              </button>
+              <img
+                className="mx-auto h-24 w-auto"
+                src="https://cdn.discordapp.com/attachments/1037406020449861724/1037797448900419614/20221104_000510_0000.png"
+                alt="Your Company"
+              />
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Sign in to your account
+              </h2>
+              <p className="mt-2 text-center text-sm text-gray-600">
+                Or{" "}
+                <Link
+                  to="/register"
+                  className="font-medium text-blue-600 hover:text-blue-500"
+                >
+                  register for new account
+                </Link>
+              </p>
             </div>
-          </form>
+            <form className="mt-8 space-y-6">
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="-space-y-px rounded-md shadow-sm">
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Email address"
+                    onChange={(e) => {
+                      setemail(e.target.value);
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Password"
+                    onChange={(e) => {
+                      setpassword(e.target.value);
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                  />
+                  <label
+                    htmlFor="remember-me"
+                    className="ml-2 block text-sm text-gray-900"
+                  >
+                    Remember me
+                  </label>
+                </div>
+
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-medium text-green-600 hover:text-green-500"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+
+              <div>
+                <button
+                  onClick={login}
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-offset-2"
+                >
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <LockClosedIcon
+                      className="h-5 w-5 text-green-200 group-hover:text-hover-500"
+                      aria-hidden="true"
+                    />
+                  </span>
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-            </div>
-        </>
-    )
+    </>
+  );
 }
 
-export default Signin
+export default Signin;
