d3352b4631024ebc369df0ff35723c6b69702b24
rishabhkr-r111 <rishabhkrr111@gmail.com>
2023-07-15 15:29:59 +0530

    bug fixes : imgs link update

--- src/assets/comp/concerns.jsx
+++ src/assets/comp/concerns.jsx
@@ -1,116 +1,145 @@
-import { Link, useNavigate} from 'react-router-dom'
-
+import { Link, useNavigate } from "react-router-dom";
 
 function Concerns() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    function nav(q) {
-      navigate('/search/' + q)
-    }
+  function nav(q) {
+    navigate("/search/" + q);
+  }
 
-    return(
-        <>
-        <div className="flex"> 
-            <div className="mt-7 mb-2 mx-9 text-xl text-white font-bold text-center p-2  rounded-[25px]
+  return (
+    <>
+      <div className="flex">
+        <div
+          className="mt-7 mb-2 mx-9 text-xl text-white font-bold text-center p-2  rounded-[25px]
               bg-black drop-shadow-xl px-7 py-2
-            ">  Shop by Concerns  </div>
+            "
+        >
+          {" "}
+          Shop by Concerns{" "}
+        </div>
+      </div>
+
+      <div className="w-full flex overflow-x-scroll flex-nowrap no-scrollbar">
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Diabetes Medicine");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285261337002074/png_20221104_232053_0000.png"
+          />
+          <div className="product-text">Diabetes</div>
         </div>
 
-        <div className="w-full flex overflow-x-scroll flex-nowrap no-scrollbar">
-
-            <div className="product-container " onClick={() => { nav('Diabetes Medicine') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284288640163930/png_20221104_232053_0000.png"
-            />
-            <div className="product-text">Diabetes</div>
-
-            </div>
-
-            <div className="product-container " onClick={() => { nav('Cardiac') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284289118318602/png_20221104_233207_0000.png"
-            />
-            <div className="product-text">Cardiac</div>
-
-            </div>
-
-            <div className="product-container " onClick={() => { nav('Gastro') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284289512591530/png_20221105_002546_0000.png"
-            />
-            <div className="product-text">Gastro Intestinal</div>
-
-            </div>
-
-            <div className="product-container " onClick={() => { nav('Liver') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284289936207883/png_20221105_073031_0000.png"
-            />
-            <div className="product-text">Liver</div>
-
-            </div>
-
-            <div className="product-container " onClick={() => { nav('Respiratory') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284290275954698/png_20221105_073903_0000.png"
-            />
-            <div className="product-text">Respiratory</div>
-
-            </div>
-
-            <div className="product-container " onClick={() => { nav('Bones and Joints') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284291047694376/png_20221105_074534_0000.png"
-            />
-            <div className="product-text">Bones, Joints And Muscles</div>
-
-            </div>
-
-            <div className="product-container " onClick={() => { nav('Ears, Nose and Throat') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284291509071972/png_20221105_081259_0000.png"
-            />
-            <div className="product-text">Ears, Nose and Throat</div>          
-
-            </div>
-
-           
-
-            <div className="product-container " onClick={() => { nav('Eye') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284292326969414/png_20221105_082022_0000.png"
-            />
-            <div className="product-text">Eye</div>          
-            </div>
-
-            <div className="product-container " onClick={() => { nav('Kidney') }}>
-            <img 
-                className="product-type"
-                src="https://cdn.discordapp.com/attachments/1038137730615484427/1038284291894943824/png_20221105_081508_0000.png"
-            />
-            <div className="product-text">Kidney</div>          
-            </div>
-
-            
-
-           
-
-
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Cardiac");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285261752242216/png_20221104_233207_0000.png"
+          />
+          <div className="product-text">Cardiac</div>
         </div>
-        
-        
-        
-        </>
-    )
 
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Gastro");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285262125539354/png_20221105_002546_0000.png"
+          />
+          <div className="product-text">Gastro Intestinal</div>
+        </div>
+
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Liver");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285262503022694/png_20221105_073031_0000.png"
+          />
+          <div className="product-text">Liver</div>
+        </div>
+
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Respiratory");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285262863728690/png_20221105_073903_0000.png"
+          />
+          <div className="product-text">Respiratory</div>
+        </div>
+
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Bones and Joints");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285263241220147/png_20221105_074534_0000.png"
+          />
+          <div className="product-text">Bones, Joints And Muscles</div>
+        </div>
+
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Ears, Nose and Throat");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285263597740075/png_20221105_081259_0000.png"
+          />
+          <div className="product-text">Ears, Nose and Throat</div>
+        </div>
+
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Eye");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285264277217280/png_20221105_082022_0000.png"
+          />
+          <div className="product-text">Eye</div>
+        </div>
+
+        <div
+          className="product-container "
+          onClick={() => {
+            nav("Kidney");
+          }}
+        >
+          <img
+            className="product-type"
+            src="https://cdn.discordapp.com/attachments/1038025122369503282/1038285263937474590/png_20221105_081508_0000.png"
+          />
+          <div className="product-text">Kidney</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Concerns
+export default Concerns;
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,94 +1,101 @@
-import { LockClosedIcon } from '@heroicons/react/20/solid'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../firebase-config'
-import { useState } from 'react'
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase-config";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
-  const [email, setemail] = useState('');
-  const [password, setpassword] = useState('');
+  const navigate = useNavigate();
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
 
   const register = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const user = await createUserWithEmailAndPassword(auth, email, password)
-      console.log(user)
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(user);
+      navigate("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-    return(
-        <>
-            <div >
-                <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
-        <div className="w-full max-w-md space-y-8 border-2 border-gray-200 rounded-xl drop-shadow-lg bg-gray-50 p-11">
-          <div>
-            <img
-              className="mx-auto h-24 w-auto"
-              src="https://cdn.discordapp.com/attachments/1037406020449861724/1037797448900419614/20221104_000510_0000.png"
-              alt="Your Company"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Register for new account
-            </h2>
-            {/* <p className="mt-2 text-center text-sm text-gray-600">
+  return (
+    <>
+      <div>
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
+          <div className="w-full max-w-md space-y-8 border-2 border-gray-200 rounded-xl drop-shadow-lg bg-gray-50 p-11">
+            <div>
+              <img
+                className="mx-auto h-24 w-auto"
+                src="https://cdn.discordapp.com/attachments/1037406020449861724/1037797448900419614/20221104_000510_0000.png"
+                alt="Your Company"
+              />
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Register for new account
+              </h2>
+              {/* <p className="mt-2 text-center text-sm text-gray-600">
               Or{' '}
               <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
                 register for new account
               </a>
             </p> */}
-          </div>
-          <form className="mt-8 space-y-6">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="-space-y-px rounded-md shadow-sm">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Email address"
-                  onChange={(e) => { setemail(e.target.value) }}
-                />
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Password"
-                  onChange={(e) => { setpassword(e.target.value) }}
-                />
-              </div>
-
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Confirm Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                  placeholder="Confirm Password"
-                />
-              </div>
             </div>
+            <form className="mt-8 space-y-6">
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="-space-y-px rounded-md shadow-sm">
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Email address"
+                    onChange={(e) => {
+                      setemail(e.target.value);
+                    }}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Password"
+                    onChange={(e) => {
+                      setpassword(e.target.value);
+                    }}
+                  />
+                </div>
 
-            {/* <div className="flex items-center justify-between">
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Confirm Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Confirm Password"
+                  />
+                </div>
+              </div>
+
+              {/* <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <input
                   id="remember-me"
@@ -108,23 +115,26 @@ function Register() {
               </div>
             </div> */}
 
-            <div>
-              <button
-                onClick={ register }
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-green-200 group-hover:text-hover-500" aria-hidden="true" />
-                </span>
-                Register
-              </button>
-            </div>
-          </form>
+              <div>
+                <button
+                  onClick={register}
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 focus:ring-offset-2"
+                >
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <LockClosedIcon
+                      className="h-5 w-5 text-green-200 group-hover:text-hover-500"
+                      aria-hidden="true"
+                    />
+                  </span>
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-            </div>
-        </>
-    )
+    </>
+  );
 }
 
-export default Register
+export default Register;
